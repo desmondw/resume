deea8b98fe9b56c4ff7ce4943721db7e1b13780f
Desmond Weindorf <desmondweindorf@gmail.com>
2017-10-21 17:58:08 -0700

    added txt

--- build.js
+++ build.js
@@ -1,10 +1,23 @@
-var markdownpdf = require("markdown-pdf")
-  , fs = require("fs")
+var fs = require("fs")
+var through2 = require('through2');
+var markdownPdf = require("markdown-pdf")
+var removeMarkdown = require("remove-markdown")
 
-var options = {
-    paperBorder: "1.4cm"
-};
+var resume = fs.createReadStream("README.md")
+var pdf = fs.createWriteStream("Resume - Desmond Weindorf.pdf")
+var txt = fs.createWriteStream("Resume - Desmond Weindorf.txt")
+var md = fs.createWriteStream("Resume - Desmond Weindorf.md")
 
-fs.createReadStream("README.md")
-  .pipe(markdownpdf(options))
-  .pipe(fs.createWriteStream("resume.pdf"))
+process.stdout.write('Building other file types...\n')
+
+// pdf
+resume.pipe(markdownPdf({ paperBorder: "1.4cm" })).pipe(pdf)
+
+// txt
+resume.pipe(through2(function(line, _, next) {
+    this.push(removeMarkdown(line.toString()) + '\n');
+    next()
+})).pipe(txt)
+
+// md
+resume.pipe(md)
