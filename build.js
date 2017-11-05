1e663aeda8c5ff49e313bc7c964bb24a5cf8ca24
Desmond Weindorf <desmondweindorf@gmail.com>
2017-11-05 09:33:32 -0800

    updated resume

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ var fs = require("fs")
 var through2 = require('through2');
 var markdownPdf = require("markdown-pdf")
 var removeMarkdown = require("remove-markdown")
+var sync = require("sync")
 
 var resume = fs.createReadStream("README.md")
 var pdf = fs.createWriteStream("Resume - Desmond Weindorf.pdf")
@@ -10,14 +11,18 @@ var md = fs.createWriteStream("Resume - Desmond Weindorf.md")
 
 process.stdout.write('Building other file types...\n')
 
-// pdf
-resume.pipe(markdownPdf({ paperBorder: "1.4cm" })).pipe(pdf)
+sync(function(){
 
-// txt
-resume.pipe(through2(function(line, _, next) {
-    this.push(removeMarkdown(line.toString()) + '\n');
-    next()
-})).pipe(txt)
+  // pdf
+  resume.pipe(markdownPdf({ paperBorder: "1.4cm" })).pipe(pdf)
 
-// md
-resume.pipe(md)
+  // txt
+  resume.pipe(through2(function(line, _, next) {
+      this.push(removeMarkdown(line.toString()) + '\n');
+      next()
+  })).pipe(txt)
+
+  // md
+  resume.pipe(md)
+
+})
